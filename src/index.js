fb14adbc5cf6e894c9325ebb75f7496abc7a0d29
rahulravidas024 <rahulravidas024@gmail.com>
2022-08-12 23:15:45 +0530

    Changes in index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://rahulravidas024:2iYJiS3c54qnkGXA@cluster0.emr6keu.mongodb.net/RahulRavidas-DB?retryWrites=true&w=majority"
+,{
+    useNewUrlParser: true
+}
+).then( () => { console.log("MongoDb is Connected") })
+.catch( err => { console.log(err) })
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {

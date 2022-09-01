acb5dd34e7998a8f7e4567c66ff9fdcc53bf3bcf
rahulravidas024 <rahulravidas024@gmail.com>
2022-09-02 01:18:25 +0530

    Assignments on Axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,6 +1,5 @@
 let axios = require("axios")
 
-
 let getStates = async function (req, res) {
 
     try {
@@ -18,7 +17,6 @@ let getStates = async function (req, res) {
         res.status(500).send({ msg: err.message })
     }
 }
-
 
 let getDistricts = async function (req, res) {
     try {
@@ -78,8 +76,138 @@ let getOtp = async function (req, res) {
     }
 }
 
+// Assignments Question 1
+const findByDistrict = async function(req, res){
+    try{
+        let districtId = req.query.district_id
+        let date = req.query.date
+
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`
+        }
+        const result = await axios(options)
+        let data = result.data
+        res.status(200).send({msg: data, status: true})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+// Assignments Question 2
+const getWeather = async function(req, res){
+    try{
+        let q = req.query.q
+        let appId = req.query.appid
+        
+        let options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appId}`
+        }
+        let result = await axios(options)
+        let data = result.data
+        res.status(200).send({msg: data, status: true})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+const getWeatherTemperature = async function(req, res){
+    try{
+        let q = req.query.q
+        let appId = req.query.appid
+        
+        let options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${appId}`
+        }
+        let result = await axios(options)
+        let data = result.data.main.temp
+        res.status(200).send({temp: data})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+const sortGivenCityByTemperature = async function(req, res){
+    try{
+        let q = req.query.q
+        //console.log(q)
+        let appId = req.query.appid
+        let emptyArray = []
+        for(let i=0; i<q.length; i++)
+        {
+            let options = {
+                method: "get",
+                url: `http://api.openweathermap.org/data/2.5/weather?q=${q[i]}&appid=${appId}`
+            }
+            let result = await axios(options)
+            let data = result.data.main.temp
+            let obj = {city: q[i], temp: data}
+            emptyArray.push(obj)
+        }
+        let sortedData = emptyArray.sort(function(a,b){ return a.temp - b.temp})
+        //console.log(sortedData)
+        res.status(200).send({data: sortedData})  
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+// Assignments Question 3
+const getAllMemes = async function(req, res){
+    try{
+        let options = {
+            method: "get",
+            url: "https://api.imgflip.com/get_memes"
+        }
+        let result = await axios(options)
+        let data = result.data
+        res.status(200).send({data: data, status: true})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+const createMeme = async function(req, res){
+    try{
+        let templateId = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let username = req.query.username
+        let password = req.query.password
+        let options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?template_id=${templateId}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+        }
+        let result = await axios(options)
+        let data = result.data
+        res.status(200).send({data: data, status: true})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+
+module.exports.findByDistrict = findByDistrict
+module.exports.getWeather = getWeather
+module.exports.getWeatherTemperature = getWeatherTemperature
+module.exports.sortGivenCityByTemperature = sortGivenCityByTemperature
+module.exports.getAllMemes = getAllMemes
+module.exports.createMeme = createMeme
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
 
-
-
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -12,8 +10,18 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
+
+//Assignments Route
+router.get("/cowin/findByDistrict", CowinController.findByDistrict)
+router.get("/getWeather", CowinController.getWeather)
+router.get("/getWeatherTemperature", CowinController.getWeatherTemperature)
+router.get("/sortGivenCityByTemperature", CowinController.sortGivenCityByTemperature)
+router.get("/getAllMemes", CowinController.getAllMemes)
+router.post("/createMeme", CowinController.createMeme)
+
+
+
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
